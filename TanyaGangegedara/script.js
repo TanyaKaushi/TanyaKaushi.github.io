5a7b238d4a10491accc910c6fbab238cc812dd85
Tanya Kaushi <tanyakaushi9@gmail.com>
2024-04-02 09:14:01 +0530

    Remove unwanted images

--- TanyaGangegedara/script.js
+++ TanyaGangegedara/script.js
@@ -37,30 +37,6 @@ let changeText = () => {
 changeText();
 setInterval(changeText,3000)
 
-// Circle skill //////////////////////////////////////////////////
- 
-const circles = document.querySelectorAll('.circle');
-circles.forEach(elem=>{
-    var dots = elem.getAttribute("data-dots");
-    var marked = elem.getAttribute("data-percent");
-    var percent = Math.floor(dots*marked/100);
-    var points = "";
-    var rotate = 360 / dots;
-
-    for(let i = 0 ; i < dots ; i++){
-        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
-    }  
-    elem.innerHTML = points;
-
-    const pointMarked = elem.querySelectorAll('.points');
-    for(let i = 0 ; i < percent ; i++){
-        pointMarked[i].classList.add('marked');
-    } 
-})
-
-
-// mix it up portfolio section
-var mixer = mixitup('.portfolio-gallery');
 
 //action menu //////////////////////////////////////////////////
 let menuLi = document.querySelectorAll('header ul li a');
